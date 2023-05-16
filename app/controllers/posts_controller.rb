2b0fb98d5f052a155aa1df46a12113bdf788a818
Jenna Chodos <jenna.chodos@kellogg.northwestern.edu>
2023-05-16 02:22:23 +0000

    started first draft of hw3 - post not saving yet

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+def show
+  @post = Post.find_by({"id"=> params["id"]})
+  @place = Place.find_by({"id" => @post["post_id"]})
+end 
+
+def new 
+  @post = Post.new 
+  @place = Place.find_by({"id" => params["place_id"]})
+  @post["place_id"]= @place["id"]
+end
+
+def create
+  @post = Post.new 
+  #assign user-entered form data to Post's columns
+  @post["title"] = params["post"]["title"]
+  @post["posted_on"]= params["post"]["posted_on"]
+  @post["description"]= params["post"]["description"]
+  #save Post row
+  @post.save
+  #redirect user
+  redirect_to "/places/#{@post["place_id"]}"
+end
+
+end
